163caa16253d3f950af1d81582de060f02749d9e
Newt Fourie <newt@kandua.com>
2024-08-29 20:28:51 +0200

    Gotta try with sudo

--- server/handlers/restart.go
+++ server/handlers/restart.go
@@ -9,7 +9,7 @@ import (
 // Restart service
 // TODO get the service name from settings
 func RestartService(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("systemctl", "restart", "yt-dl")
+	cmd := exec.Command("sudo", "systemctl", "restart", "yt-dl")
 	err := cmd.Run()
 	if err != nil {
 		http.Error(w, "Failed to create file: "+err.Error(), http.StatusInternalServerError)
