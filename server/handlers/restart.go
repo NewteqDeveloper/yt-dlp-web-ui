1736d09058767150f09293d4ff4b1a976d9ce3c9
Newt Fourie <newt@kandua.com>
2024-08-29 20:15:08 +0200

    Use touch instead for example

--- server/handlers/restart.go
+++ server/handlers/restart.go
@@ -3,14 +3,14 @@ package handlers
 import (
 	"net/http"
 
-	"os"
+	//"os"
 	"os/exec"
 )
 
 // Restart service
 func RestartService(w http.ResponseWriter, r *http.Request) {
-	filePath := "/app/new-file"
-	err := touchFile(filePath)
+	//filePath := "/app/new-file"
+	err := touchFile()
 	if err != nil {
 		http.Error(w, "Failed to create file: "+err.Error(), http.StatusInternalServerError)
 		return
@@ -19,28 +19,16 @@ func RestartService(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Service restarted!"))
 }
-// func RestartService() http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		filePath := "/app/new-file"
-// 		err := touchFile(filePath)
-// 		if err != nil {
-// 			http.Error(w, "Failed to create file: "+err.Error(), http.StatusInternalServerError)
-// 			return
-// 		}
-
-// 		w.WriteHeader(http.StatusOK)
-// 		w.Write([]byte("Service restarted!"))
-// 	}
-// }
 
 // Helper function to "touch" a file
-func touchFile(filePath string) error {
+//func touchFile(filePath string) error {
+func touchFile() error {
 	// Use os to create the file if it doesn't exist or update the modification time
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		_, err := os.Create(filePath)
-		return err
-	}
+	// if _, err := os.Stat(filePath); os.IsNotExist(err) {
+	// 	_, err := os.Create(filePath)
+	// 	return err
+	// }
 	// Use the "touch" command if you prefer to directly call it
-	cmd := exec.Command("touch", filePath)
+	cmd := exec.Command("touch", "/app/touch.me.wtf")
 	return cmd.Run()
 }
