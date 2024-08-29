6870838661d4436fe459c1a19a6f544ed2ec16cb
Newt Fourie <newt@kandua.com>
2024-08-29 20:17:28 +0200

    Make service restart actually

--- server/handlers/restart.go
+++ server/handlers/restart.go
@@ -3,14 +3,14 @@ package handlers
 import (
 	"net/http"
 
-	//"os"
 	"os/exec"
 )
 
 // Restart service
+// TODO get the service name from settings
 func RestartService(w http.ResponseWriter, r *http.Request) {
-	//filePath := "/app/new-file"
-	err := touchFile()
+	cmd := exec.Command("systemctl", "restart", "yt-dl")
+	err := cmd.Run()
 	if err != nil {
 		http.Error(w, "Failed to create file: "+err.Error(), http.StatusInternalServerError)
 		return
@@ -18,17 +18,4 @@ func RestartService(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Service restarted!"))
-}
-
-// Helper function to "touch" a file
-//func touchFile(filePath string) error {
-func touchFile() error {
-	// Use os to create the file if it doesn't exist or update the modification time
-	// if _, err := os.Stat(filePath); os.IsNotExist(err) {
-	// 	_, err := os.Create(filePath)
-	// 	return err
-	// }
-	// Use the "touch" command if you prefer to directly call it
-	cmd := exec.Command("touch", "/app/touch.me.wtf")
-	return cmd.Run()
 }
