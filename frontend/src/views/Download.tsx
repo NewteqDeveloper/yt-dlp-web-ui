924962975dc7f535a46823d37c5a889555b23795
Newt Fourie <newt@kandua.com>
2024-09-11 22:13:43 +0200

    The reason nothing new was showing was a PEBKAC issue. Raspberry.build.sh will fix this

--- frontend/src/views/Download.tsx
+++ frontend/src/views/Download.tsx
@@ -2,10 +2,9 @@ import { Container } from '@mui/material';
 import DownloadPersist from '../components/DownloadPersist';
 
 export default function Download() {
-  return <div style={{ fontSize: 36 }}>Hello</div>;
-  // return (
-  //   <Container maxWidth="xl" sx={{ mt: 2, mb: 8 }}>
-  //     <DownloadPersist />
-  //   </Container>
-  // );
+  return (
+    <Container maxWidth="xl" sx={{ mt: 2, mb: 8 }}>
+      <DownloadPersist />
+    </Container>
+  );
 }
