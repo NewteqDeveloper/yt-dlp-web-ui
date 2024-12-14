016c4045b1fb9b03ff38286b3f46832b41429cd0
Newt Fourie <newt@kandua.com>
2024-12-14 14:52:58 +0200

    Very dumb mistake

--- frontend/src/atoms/settings.ts
+++ frontend/src/atoms/settings.ts
@@ -1,9 +1,9 @@
-import { pipe } from 'fp-ts/lib/function'
-import { matchW } from 'fp-ts/lib/TaskEither'
-import { ffetch } from '../lib/httpClient'
-import { prefersDarkMode } from '../utils'
-import { atomWithStorage } from 'jotai/utils'
-import { atom } from 'jotai'
+import { pipe } from 'fp-ts/lib/function';
+import { matchW } from 'fp-ts/lib/TaskEither';
+import { ffetch } from '../lib/httpClient';
+import { prefersDarkMode } from '../utils';
+import { atomWithStorage } from 'jotai/utils';
+import { atom } from 'jotai';
 
 export const languages = [
   'english',
@@ -18,144 +18,147 @@ export const languages = [
   'ukrainian',
   'swedish',
   'polish',
-  'german'
-] as const
+  'german',
+] as const;
 
-export type Language = (typeof languages)[number]
+export type Language = (typeof languages)[number];
 
-export type Theme = 'light' | 'dark' | 'system'
-export type ThemeNarrowed = 'light' | 'dark'
+export type Theme = 'light' | 'dark' | 'system';
+export type ThemeNarrowed = 'light' | 'dark';
 
-export const accents = ['default', 'red'] as const
-export type Accent = (typeof accents)[number]
+export const accents = ['default', 'red'] as const;
+export type Accent = (typeof accents)[number];
 
 export interface SettingsState {
-  serverAddr: string
-  serverPort: number
-  language: Language
-  theme: ThemeNarrowed
-  accent: Accent
-  cliArgs: string
-  formatSelection: boolean
-  fileRenaming: boolean
-  pathOverriding: boolean
-  enableCustomArgs: boolean
-  listView: boolean
-  servedFromReverseProxy: boolean
-  appTitle: string
+  serverAddr: string;
+  serverPort: number;
+  language: Language;
+  theme: ThemeNarrowed;
+  accent: Accent;
+  cliArgs: string;
+  formatSelection: boolean;
+  fileRenaming: boolean;
+  pathOverriding: boolean;
+  enableCustomArgs: boolean;
+  listView: boolean;
+  servedFromReverseProxy: boolean;
+  appTitle: string;
 }
 
 export const languageState = atomWithStorage<Language>(
   'language',
-  localStorage.getItem('language') as Language || 'english'
-)
+  (localStorage.getItem('language') as Language) || 'english'
+);
 
 export const themeState = atomWithStorage<Theme>(
   'theme',
-  localStorage.getItem('theme') as Theme || 'system'
-)
+  (localStorage.getItem('theme') as Theme) || 'system'
+);
 
 export const serverAddressState = atomWithStorage<string>(
   'server-addr',
   localStorage.getItem('server-addr') || window.location.hostname
-)
+);
 
 export const serverPortState = atomWithStorage<number>(
   'server-port',
   Number(localStorage.getItem('server-port')) || Number(window.location.port)
-)
+);
 
 export const latestCliArgumentsState = atomWithStorage<string>(
   'cli-args',
   localStorage.getItem('cli-args') || '--no-mtime'
-)
+);
 
 export const formatSelectionState = atomWithStorage(
   'format-selection',
   localStorage.getItem('format-selection') === 'true'
-)
+);
 
 export const fileRenamingState = atomWithStorage(
   'file-renaming',
   localStorage.getItem('file-renaming') === 'true'
-)
+);
 
 export const pathOverridingState = atomWithStorage(
   'path-overriding',
   localStorage.getItem('path-overriding') === 'true'
-)
+);
 
 export const enableCustomArgsState = atomWithStorage(
   'enable-custom-args',
   localStorage.getItem('enable-custom-args') === 'true'
-)
+);
 
 export const listViewState = atomWithStorage(
   'listview',
   localStorage.getItem('listview') === 'true'
-)
+);
 
 export const servedFromReverseProxyState = atomWithStorage(
   'reverseProxy',
   localStorage.getItem('reverseProxy') === 'true' || window.location.port == ''
-)
+);
 
 export const servedFromReverseProxySubDirState = atomWithStorage<string>(
   'reverseProxySubDir',
   localStorage.getItem('reverseProxySubDir') ?? ''
-)
+);
 
 export const appTitleState = atomWithStorage(
   'appTitle',
-  localStorage.getItem('appTitle') ?? 'Sanity's YouTube Downloader'
-)
+  localStorage.getItem('appTitle') ?? `Sanity's YouTube Downloader`
+);
 
 export const serverAddressAndPortState = atom((get) => {
   if (get(servedFromReverseProxySubDirState)) {
-    return `${get(serverAddressState)}/${get(servedFromReverseProxySubDirState)}/`
+    return `${get(serverAddressState)}/${get(
+      servedFromReverseProxySubDirState
+    )}/`;
   }
   if (get(servedFromReverseProxyState)) {
-    return `${get(serverAddressState)}`
+    return `${get(serverAddressState)}`;
   }
-  return `${get(serverAddressState)}:${get(serverPortState)}`
-})
+  return `${get(serverAddressState)}:${get(serverPortState)}`;
+});
 
-export const serverURL = atom((get) =>
-  `${window.location.protocol}//${get(serverAddressAndPortState)}`
-)
+export const serverURL = atom(
+  (get) => `${window.location.protocol}//${get(serverAddressAndPortState)}`
+);
 
 export const rpcWebSocketEndpoint = atom((get) => {
-  const proto = window.location.protocol === 'https:' ? 'wss:' : 'ws:'
-  return `${proto}//${get(serverAddressAndPortState)}/rpc/ws`
-}
-)
+  const proto = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+  return `${proto}//${get(serverAddressAndPortState)}/rpc/ws`;
+});
 
 export const rpcHTTPEndpoint = atom((get) => {
-  const proto = window.location.protocol
-  return `${proto}//${get(serverAddressAndPortState)}/rpc/http`
-}
-)
+  const proto = window.location.protocol;
+  return `${proto}//${get(serverAddressAndPortState)}/rpc/http`;
+});
 
-export const serverSideCookiesState = atom<Promise<string>>(async (get) => await pipe(
-  ffetch<Readonly<{ cookies: string }>>(`${get(serverURL)}/api/v1/cookies`),
-  matchW(
-    () => '',
-    (r) => r.cookies
-  )
-)())
+export const serverSideCookiesState = atom<Promise<string>>(
+  async (get) =>
+    await pipe(
+      ffetch<Readonly<{ cookies: string }>>(`${get(serverURL)}/api/v1/cookies`),
+      matchW(
+        () => '',
+        (r) => r.cookies
+      )
+    )()
+);
 
 const themeSelector = atom<ThemeNarrowed>((get) => {
-  const theme = get(themeState)
+  const theme = get(themeState);
   if ((theme === 'system' && prefersDarkMode()) || theme === 'dark') {
-    return 'dark'
+    return 'dark';
   }
-  return 'light'
-})
+  return 'light';
+});
 
 export const accentState = atomWithStorage<Accent>(
   'accent-color',
-  localStorage.getItem('accent-color') as Accent ?? 'default',
-)
+  (localStorage.getItem('accent-color') as Accent) ?? 'default'
+);
 
 export const settingsState = atom<SettingsState>((get) => ({
   serverAddr: get(serverAddressState),
@@ -170,6 +173,5 @@ export const settingsState = atom<SettingsState>((get) => ({
   enableCustomArgs: get(enableCustomArgsState),
   listView: get(listViewState),
   servedFromReverseProxy: get(servedFromReverseProxyState),
-  appTitle: get(appTitleState)
-})
-)
+  appTitle: get(appTitleState),
+}));
